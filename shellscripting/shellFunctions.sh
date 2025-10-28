1f2e70c1562c22cde737d3b4359eafdb5d49cda1
devops_01 <devops_01@gmail.com>
2025-10-28 20:40:22 +0530

    Script for function and loop

---
+++ shellscripting/shellFunctions.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+echo "Script for the below topics: "
+echo "Simple for-loop (list, range, i++)"
+echo "For-loop (read start, end, increment from user's input)"
+echo "Simple while-loop ((read start, end, increment from user's input))"
+echo "basic function"
+echo "function with parameters"
+echo "function with user's input"
