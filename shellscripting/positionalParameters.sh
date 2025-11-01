3e635ef28812b1b3378a6e740bd28f63a635856a
devops_01 <devops_01@gmail.com>
2025-11-01 17:17:15 +0530

    Shell positional parameters

---
+++ shellscripting/positionalParameters.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+#this script explain how positional parameters
+echo "Script Name: $0"
+echo "First Command-line Argument: $1"
+echo "Second Command-line Argument: $2"
+echo "Third Command-line Argument: $3"
+echo "All Command-line Arguments: $@"
+echo "Total Number of Command-line Arguments: $#"
