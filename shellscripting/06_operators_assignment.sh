58f4d3cdd1bb9e9c848b52b6fb0ca45a192a5a68
devops_01 <devops_01@gmail.com>
2025-11-01 23:19:14 +0530

    06_operators_assignment.sh has been created.

---
+++ shellscripting/06_operators_assignment.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+#This script demonstrates how assignment operator behave in a shell script.
+echo -e "\nThis script demonstrates how assignment operator behave in a shell script\n"
+
+name=Srinivas
+
+echo "$name"
