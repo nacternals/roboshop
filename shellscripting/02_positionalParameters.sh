97e308dc98eeb95e5792e61155d3100388466ee4
devops_01 <devops_01@gmail.com>
2025-11-01 17:29:39 +0530

    Positional Parameters file content has been modified...

--- shellscripting/02_positionalParameters.sh
+++ shellscripting/02_positionalParameters.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 #this script explain how positional parameters
-echo "Script Name: $0"
-echo "First Command-line Argument: $1"
-echo "Second Command-line Argument: $2"
-echo "Third Command-line Argument: $3"
-echo "All Command-line Arguments: $@"
-echo "Total Number of Command-line Arguments: $#"
+echo -e "Script Name: $0\n"
+echo -e "First Command-line Argument: $1\n"
+echo -e "Second Command-line Argument: $2\n"
+echo -e "Third Command-line Argument: $3\n"
+echo -e "All Command-line Arguments: $@\n"
+echo -e "Total Number of Command-line Arguments: $#\n"
