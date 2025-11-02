fb93b6555c96c90fd91f3c8fed205660b8893daf
devops_01 <devops_01@gmail.com>
2025-11-02 12:16:54 +0530

    89_install_packages content has been modified.

---
+++ shellscripting/89_install_packages.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+#This script demonstrates how to install packages with the help of:
+#if
+#if-else
+#if-elif-else
+
+echo -e "This script demonstrates how to install packages (git vim wget net-tools )with the help of:
+#if
+#if-else
+#if-elif-else"
+
+#Installing git
+echo -e "\nInstalling git"
+echo -e "\nChecking whether git is already installed or not"
+
+# 1. Is current user is root. If not, change to root suer
+# If yes, proceed to further
+# 2. Is git already installed. If not, install git
+# If yes, show to user that git is already installed and exit.
+
+userID=$(id -u)
+if (($userID != 0)); then
+	echo "Insufficient access; please change to root access"
+	exit 1
+else
+	echo "Sufficient root access is present; please proceed further to install the required packages."
+	yum -q list installed git
+	if (($? != 0)); then
+		yum install git -y
+		if(($? !=0)); then
+		echo "Git has been installed successfully."
+		fi
+
+	else
+		echo "Git already installed."
+	fi
+fi
