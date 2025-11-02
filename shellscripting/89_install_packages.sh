9f5c5b51c45f26c8f4872a9ba98dbd4f36ca4162
devops_01 <devops_01@gmail.com>
2025-11-02 16:42:13 +0530

    install_packages.sh content has been modified....

--- shellscripting/89_install_packages.sh
+++ shellscripting/89_install_packages.sh
@@ -25,14 +25,36 @@ if (($userID != 0)); then
 	exit 1
 else
 	echo "Sufficient root access is present; please proceed further to install the required packages."
-	yum -q list installed git &> /dev/null
+	yum -q list installed git &>/dev/null
 	if (($? != 0)); then
 		yum install git -y
-		if(($? !=0)); then
-		echo "Git has been installed successfully."
+		if (($? != 0)); then
+			echo "Git has been installed successfully."
 		fi
 
 	else
 		echo "Git already installed."
+	fi
+fi
+
+#Installing nginx
+echo -e "\nInstalling nginx"
+echo -e "\nChecking whether nginx is already installed or not"
+
+userID=$(id -u)
+if (($userID != 0)); then
+	echo "Insufficient access; please change to root access"
+	exit 1
+else
+	echo "Sufficient root access is present; please proceed further to install the required packages."
+	yum -q list installed nginx &>/dev/null
+	if (($? != 0)); then
+		yum install nginx -y
+		if (($? != 0)); then
+			echo "Nginx has been installed successfully."
+		fi
+
+	else
+		echo "Nginx has already been installed."
 	fi
 fi
