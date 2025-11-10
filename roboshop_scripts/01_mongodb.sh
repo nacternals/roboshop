8097010af0001138361d6d000580dad204aa306d
devops_01 <devops_01@gmail.com>
2025-11-10 23:48:42 +0530

    01_mongodb.sh has been created...

---
+++ roboshop_scripts/01_mongodb.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+#This script demonstrates how to install and configure MongoDB Microservice
+echo "This script demonstrates how to install and configure MongoDB Microservice"
+
