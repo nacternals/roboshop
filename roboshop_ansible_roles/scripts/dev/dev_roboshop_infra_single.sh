251c2522c56517bf017fb79fb5ac4f3a4855297a
devops_01 <devops_01@gmail.com>
2025-12-30 18:51:47 +0530

    dev_roboshop_infra_single.sh has been modified for shipping....

--- roboshop_ansible_roles/scripts/dev/dev_roboshop_infra_single.sh
+++ roboshop_ansible_roles/scripts/dev/dev_roboshop_infra_single.sh
@@ -21,12 +21,12 @@ HOSTED_ZONE_ID="Z06046792KQ5HDP2YEDR4" # TODO: Route53 hosted zone ID for optimu
 DOMAIN_NAME="optimusprime.sbs"
 
 # Microservices list
-SERVICES=(user.dev)
+SERVICES=(shipping.dev)
 
 # Instance type mapping
 declare -A INSTANCE_TYPES
 INSTANCE_TYPES=(
-	[user.dev]="t2.micro"
+	[shipping.dev]="t3.medium"
 
 )
 
