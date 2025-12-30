2e07e5a10d387ecfb6d8739b997a56364118dc67
devops_01 <devops_01@gmail.com>
2025-12-30 18:29:18 +0530

    dev_roboshop_infra_single.sh has been modified for user....

--- roboshop_ansible_roles/scripts/dev/dev_roboshop_infra_single.sh
+++ roboshop_ansible_roles/scripts/dev/dev_roboshop_infra_single.sh
@@ -21,12 +21,12 @@ HOSTED_ZONE_ID="Z06046792KQ5HDP2YEDR4" # TODO: Route53 hosted zone ID for optimu
 DOMAIN_NAME="optimusprime.sbs"
 
 # Microservices list
-SERVICES=(cart.dev)
+SERVICES=(user.dev)
 
 # Instance type mapping
 declare -A INSTANCE_TYPES
 INSTANCE_TYPES=(
-	[cart.dev]="t2.micro"
+	[user.dev]="t2.micro"
 
 )
 
