73ee886c251594249a67f7acc79f2596240e28d2
devops_01 <devops_01@gmail.com>
2025-12-30 17:02:24 +0530

    dev_roboshop_infra_single.sh has been modified for mysql....

--- roboshop_ansible_roles/scripts/dev/dev_roboshop_infra_single.sh
+++ roboshop_ansible_roles/scripts/dev/dev_roboshop_infra_single.sh
@@ -21,12 +21,12 @@ HOSTED_ZONE_ID="Z06046792KQ5HDP2YEDR4" # TODO: Route53 hosted zone ID for optimu
 DOMAIN_NAME="optimusprime.sbs"
 
 # Microservices list
-SERVICES=(redis.dev)
+SERVICES=(mysql.dev)
 
 # Instance type mapping
 declare -A INSTANCE_TYPES
 INSTANCE_TYPES=(
-	[redis.dev]="t3.medium"
+	[mysql.dev]="t3.medium"
 
 )
 
