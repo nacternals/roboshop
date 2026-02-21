932de3f2fa3406accc429748f01d1c568609979d
devops_01 <devops_01@gmail.com>
2026-02-21 15:30:19 +0530

    aaaa

--- roboshop_terraform/variables.tf
+++ roboshop_terraform/variables.tf
@@ -1,4 +1,18 @@
 variable "aws_region" {
   type = string
-  
+}
+
+variable "project" {
+  type        = string
+  description = "Project name"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment name (dev/stage/prod)"
+}
+
+variable "vpc_cidr" {
+  type        = string
+  description = "CIDR block for VPC"
 }
